7d6907694aa0075ca6342d7a98f30316e7c7b30b
Raeleen Watson <raeleenwatson@Raeleens-MacBook-Pro.local>
2018-10-21 22:13:31 -0700

    First one

---
+++ Canvas/Canvas/CanvasViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CanvasViewController.swift
+//  Canvas
+//
+//  Created by Raeleen Watson on 10/21/18.
+//  Copyright © 2018 Raeleen Watson. All rights reserved.
+//
+
+import UIKit
+
+class CanvasViewController: UIViewController {
+    
+    @IBOutlet weak var trayView: UIView!
+    
+    var trayOriginalCenter: CGPoint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
